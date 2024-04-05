876ce99ea063e3198bf8c15427421f428d9b231c
shreyasraj-001 <shreyasraj735@gmail.com>
2024-04-05 21:12:09 +0530

    1st Comment

---
+++ First.java
@@ -0,0 +1,13 @@
+class Hello{
+    public static void main(String args[]){
+        int a, int b, int sum = 0;
+        int a = 4;
+        int b = 6;
+        
+        sum = a+b;
+         System.out.println("Sum" +sum);
+         
+
+
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,20 @@
+class Hello{
+    public static void main(String args[]){
+        int x = 7;
+       
+        if(x >=6){
+            System.out.println("Hello Vikash");
+
+        }
+        else{
+            System.out.println("Good Bye");
+            
+        }
+
+
+
+        
+
+
+    }
+}
